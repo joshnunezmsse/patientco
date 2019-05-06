3b8c8facd73cd777d7115db94b84b03cc8d8789b
Josh Nunez <josh.nunez.msse@gmail.com>
2019-05-06 00:58:28 -0400

    Added portfolio list object

---
+++ PortfolioList.js
@@ -0,0 +1,138 @@
+window.addEventListener("load", function () {
+    var PortfolioList = function (e) {
+        var requiredSources = 4;
+        var sourceCount = 0;
+    
+        console.log(e.clientList);
+
+        loadResource(e.attributes["data-countryList"].value, function(d) {
+            e.CountryData = JSON.parse(d.responseText);
+            sourceCount++;
+            if (sourceCount >= requiredSources) {
+                loadUI(e);
+            }
+        });
+    
+        loadResource(e.attributes["data-currencyList"].value, function(d) {
+            e.CurrencyData = JSON.parse(d.responseText);
+            sourceCount++;
+            if (sourceCount >= requiredSources) {
+                loadUI(e);
+            }
+        });
+    
+        loadResource(e.attributes["data-clientList"].value, function(d) {
+            e.ClientData = JSON.parse(d.responseText).portfolios;
+            sourceCount++;
+            if (sourceCount >= requiredSources) {
+                loadUI(e);
+            }
+        });
+
+        loadResource(e.attributes["data-exchangeRateList"].value, function(d) {
+            e.ExchangeRates = JSON.parse(d.responseText).rates;
+            sourceCount++;
+            if (sourceCount >= requiredSources) {
+                loadUI(e);
+            }            
+        })
+    }
+
+    var loadUI = function (e) {
+        var newClient = e.querySelector(".client");
+        var clientTemplate = newClient.cloneNode(true);
+        var usdSymbol = getCountryCurrency(e, "USD").symbol;
+
+        for(var i=0; i < e.ClientData.length; i++) {
+            newClient.countryInfo = getCountry(e, e.ClientData[i].countryCode);
+            newClient.countryInfo.currency = getCountryCurrency(e, newClient.countryInfo.currencyCode);
+            newClient.portfolioValue = calculatePortfolioValue(e, e.ClientData[i]);
+            newClient.querySelector("#client-name").innerHTML = e.ClientData[i].name;
+            newClient.querySelector("#client-nationality").innerHTML = newClient.countryInfo.name + " (" + newClient.countryInfo.currency.symbol + ")";
+            newClient.querySelector("#portfolio-value").innerHTML = usdSymbol + " " + newClient.portfolioValue;
+            insertClient(e, newClient);
+            newClient = clientTemplate.cloneNode(true);
+        }
+    }
+
+    var insertClient = function(e, client) {
+        var done = false;
+        var clientList = e.querySelectorAll(".client");
+        var listLength = clientList.length;
+
+        if (listLength <= 0) {
+            e.appendChild(client);
+            done = true;
+        } else if (clientList[0].portfolioValue > client.portfolioValue) {
+            clientList[0].insertAdjacentElement("beforebegin", client);
+            done = true;
+        } else if (listLength == 1) {
+            clientList[0].insertAdjacentElement("afterend", client);
+            done = true;
+        }
+
+        for (var i=0; i < listLength - 1 && !done; i++) {
+            if (clientList[i].portfolioValue <= client.portfolioValue &&
+                client.portfolioValue < clientList[i+1].portfolioValue) {
+                clientList[i].insertAdjacentElement("afterend", client);
+                done = true;
+            }
+        }
+
+        if (!done) {
+            if (clientList[i].portfolioValue > client.portfolioValue) {
+                clientList[i].insertAdjacentElement("beforebegin", client)
+            } else {
+                clientList[i].insertAdjacentElement("afterend", client)
+            }
+            done = true;
+        }
+    }
+
+    var calculatePortfolioValue = function (e, client) {
+        var returnValue = 0;
+        for (var i=0; i < client.fxAssets.length; i++) {
+            var factor = e.ExchangeRates[client.fxAssets[i].currencyCode]
+            returnValue += client.fxAssets[i].amount * factor;
+        }
+        return Math.round(returnValue);
+    }
+
+    var getCountry = function (e, code) {
+        var returnCountry = null;
+        for (var i=0; i < e.CountryData.length && !returnCountry; i++) {
+            if (e.CountryData[i].code === code) {
+                returnCountry = e.CountryData[i];
+            }
+        }
+        return returnCountry;
+    }
+
+    var getCountryCurrency = function(e, code) {
+        var returnCurrency = null;
+        for (var i=0; i < e.CurrencyData.length && !returnCurrency; i++) {
+            if (e.CurrencyData[i].isoCode === code) {
+                returnCurrency = e.CurrencyData[i];
+            }
+        }
+        return returnCurrency;
+    }
+
+    var loadResource = function(fileName, callback) {
+        var request = new XMLHttpRequest();
+        request.overrideMimeType("application/json");
+        request.open("GET", fileName, true);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == "200") {
+                callback(request)
+            }
+        }
+        request.send();
+    };
+
+    // only the first portfoliolist on the document will get functionality
+    var portfolioElement = document.querySelector(".PortfolioList");
+    if (portfolioElement) {
+        PortfolioList(portfolioElement);
+    }
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # patientco
+
+All total portfolio values are presented in USD because the openexchangerates.org account provided only has USD base currency available.
